2b24a49f74ece0431194a6826da026f46ae99c9d
oda29 <57302612+oda29@users.noreply.github.com>
2019-12-03 23:03:00 +0200

    Add files via upload

--- Main.cpp
+++ Main.cpp
@@ -1,13 +1,40 @@
 #include <SFML/Graphics.hpp>
 #include<iostream>
+#include<vector>
 
 sf::Clock runTime;
 
-void startLerp(bool &shouldLerp, float &timeStartedLerping)
+//Class that loads the textures
+struct Textures
 {
-	timeStartedLerping = runTime.getElapsedTime().asSeconds();
-	shouldLerp = true;
-}
+	Textures()
+	{
+		approachCircleTexture.setSmooth(true);
+		hitCircleTexture.setSmooth(true);
+#ifdef _WIN32
+		approachCircleTexture.loadFromFile("E:/visualproj/SFMLosuBootleg/skins/approachcircle.png");
+		hitCircleTexture.loadFromFile("E:/visualproj/SFMLosuBootleg/skins/hitcircle.png");
+#else
+#ifdef __linux__
+		approachCircleTexture.loadFromFile("/root/Documents/SFMLosu!/SFMLosu/skins/approachcircle.png");
+		hitCircleTexture.loadFromFile("/root/Documents/SFMLosu!/SFMLosu/skins/hitcircle.png");
+#endif
+#endif
+	}
+	sf::Texture approachCircleTexture;
+	sf::Texture hitCircleTexture;
+};
+
+class HitCircleSprites
+{
+public:
+	HitCircleSprites()
+	{
+
+	}
+private:
+	//sf::Texture 
+};
 
 void setOriginAndReadjust(sf::Sprite &object, const sf::Vector2f &newOrigin)
 {
@@ -16,52 +43,67 @@ void setOriginAndReadjust(sf::Sprite &object, const sf::Vector2f &newOrigin)
 	object.setPosition(400.0f, 400.0f);
 }
 
+class ApproachCirclesSprites : Textures
+{
+private:
+	sf::Sprite approachCircle;
+	float approachSpeed;
+public:
+	ApproachCirclesSprites(float approachSpeed,sf::Vector2f xy)
+	{
+		this->approachCircle.setTexture(approachCircleTexture);
+		this->approachCircle.setScale(2.0f, 2.0f);
+		//Set the origin to center of the circle, move the circle there======
+		this->approachCircle.setOrigin(xy);
+		this->approachCircle.move(xy - this->approachCircle.getOrigin());
+		//===================================================================
+		this->approachCircle.setPosition(400.0f, 400.0f);
+		this->approachSpeed = approachSpeed;
+	}
+
+	float getAS()
+	{
+		return this->approachSpeed;
+	}
+
+	void approachTheCircle(sf::Vector2f AR)
+	{
+		this->approachCircle.setScale(this->approachCircle.getScale() - AR);
+	}
+
+	sf::Sprite getApproachCircle()
+	{
+		return this->approachCircle;
+	}
+
+};
+
+void startLerp(bool &shouldLerp, float &timeStartedLerping)
+{
+	timeStartedLerping = runTime.getElapsedTime().asSeconds();
+	shouldLerp = true;
+}
+
 int main()
 {
 	sf::RenderWindow window(sf::VideoMode(1920, 1080), "osu!", sf::Style::Fullscreen);
-
-	//Textures used==================================================================================
-	sf::Texture approachCircleTexture;
-	approachCircleTexture.setSmooth(true);
-
-	sf::Texture number1Texture;
-	number1Texture.setSmooth(true);
-
-	sf::Texture hitCircleTexture;
-	hitCircleTexture.setSmooth(true);
-
-#ifdef _WIN32
-	approachCircleTexture.loadFromFile("E:/visualproj/SFMLosuBootleg/skins/approachcircle.png");
-	hitCircleTexture.loadFromFile("E:/visualproj/SFMLosuBootleg/skins/hitcircle.png");
-#else
-#ifdef __linux__
-	approachCircleTexture.loadFromFile("/root/Documents/SFMLosu!/SFMLosu/skins/approachcircle.png");
-	hitCircleTexture.loadFromFile("/root/Documents/SFMLosu!/SFMLosu/skins/hitcircle.png");
-#endif
-#endif
-
-	//================================================================================================
-
+	Textures textures;
 	//Sprites used====================================================================================
-	sf::Sprite approachCircle;
-	approachCircle.setTexture(approachCircleTexture);
-	approachCircle.setScale(2.0f, 2.0f);
+	ApproachCirclesSprites approachCircle(3.0, { 70,70 });
 
 	sf::Sprite hitCircle;
-	hitCircle.setTexture(hitCircleTexture);
+	hitCircle.setTexture(textures.hitCircleTexture);
 
 	//================================================================================================
 
 	setOriginAndReadjust(hitCircle, { 64,64 });
-	setOriginAndReadjust(approachCircle, { 70,70 });
 
 	//Other utilities=================================================================================
 	sf::Clock deltaClock;
 	sf::Time deltaTime;
 	bool shouldLerp = false;
 	float timeStartedLerping;
-	float x = approachCircle.getScale().x;
-	float timer = 0.0f;
+	sf::Vector2f x = approachCircle.getApproachCircle().getScale();
 	//================================================================================================
 
 	startLerp(shouldLerp,timeStartedLerping);
@@ -76,17 +118,16 @@ int main()
 		//Render stuff to screen ====================================================
 		window.clear();
 
-		if (approachCircle.getScale().x<=1.0f){ }
+		if (approachCircle.getApproachCircle().getScale().x<=1.0f){ }
 		else
 		{
-			if (approachCircle.getScale().x <= hitCircle.getScale().x){ }
+			if (approachCircle.getApproachCircle().getScale().x <= hitCircle.getScale().x){ }
 			else
 				std::cout << runTime.getElapsedTime().asSeconds()-timeStartedLerping<<std::endl;
-			timer = 0.9f;
-			approachCircle.setScale(approachCircle.getScale().x - ((x - hitCircle.getScale().x)/timer)*deltaTime.asSeconds(), approachCircle.getScale().x - ((x - hitCircle.getScale().x)/timer)*deltaTime.asSeconds());
+			approachCircle.approachTheCircle(((x - hitCircle.getScale())/approachCircle.getAS())*deltaTime.asSeconds());
 		}
 		
-		window.draw(approachCircle);
+		window.draw(approachCircle.getApproachCircle());
 		window.draw(hitCircle);
 
 		window.display();
