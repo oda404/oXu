d079f395ee503d918bbfb31f8654456f1cf424b9
oda29 <odagaming29@gmail.com>
2020-03-09 18:58:50 -0400

    Restructure 3rdPartyDependencies folder

--- dependencies.sh
+++ dependencies.sh
@@ -1,4 +1,5 @@
 sudo apt-get install libsfml-dev && sudo apt-get install libpthread-stubs0-dev && sudo apt-get install libboost-all-dev
-sudo cp 3rdPartyDependencies/libbass.so /usr/lib
+cp 3rdPartyDependencies/lib/libbass.so /usr/lib
+cp 3rdPartyDependencies/include/bass.h /usr/include
 mkdir songs
 echo "...Done!"
