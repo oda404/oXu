aef9da7ee3dd814e350d37ef08976bb8718c3822
oda404 <odagaming29@gmail.com>
2020-04-26 16:44:05 +0300

    Delete old stuff

--- dependencies.sh
+++
@@ -1,5 +0,0 @@
-sudo apt-get install libsfml-dev && sudo apt-get install libpthread-stubs0-dev && sudo apt-get install libboost-all-dev
-cp 3rdPartyDependencies/lib/libbass.so /usr/lib
-cp 3rdPartyDependencies/include/bass.h /usr/include
-mkdir songs
-echo "...Done!"
