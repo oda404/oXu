3e41aba4584805fc6ed7fef36699d40ef8ba8ecb
oda404 <57302612+oda404@users.noreply.github.com>
2020-05-14 01:43:36 +0300

    Update deps.sh

--- deps.sh
+++ deps.sh
@@ -1,6 +1,7 @@
-sudo apt-get install libsdl2-dev &&
-sudo apt-get install libsdl2-image-dev &&
-sudo apt-get install libsdl2-mixer-dev
+sudo apt-get update &&
+sudo apt-get install -y libsdl2-dev &&
+sudo apt-get install -y libsdl2-image-dev &&
+sudo apt-get install -y libsdl2-mixer-dev
 
 mkdir songs
 
