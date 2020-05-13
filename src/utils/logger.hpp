4ef0badcebd6fffdfcc5421c6dd5bae4e4a3f726
oda404 <odagaming29@gmail.com>
2020-05-13 17:24:42 +0300

    Define LOG_DEBUG

--- src/utils/logger.hpp
+++ src/utils/logger.hpp
@@ -18,7 +18,8 @@ namespace oxu
         static std::shared_ptr<spdlog::logger> &getLogger();
     };
 
-    #define LOG_INFO Logger::getLogger()->info
-    #define LOG_WARN Logger::getLogger()->warn
-    #define LOG_ERR  Logger::getLogger()->error
+    #define LOG_INFO  Logger::getLogger()->info
+    #define LOG_WARN  Logger::getLogger()->warn
+    #define LOG_ERR   Logger::getLogger()->error
+    #define LOG_DEBUG Logger::getLogger()->debug
 }
