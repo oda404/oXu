9d10ef2fe7ce2ee31c8d79343879d0dc073d69e9
oda404 <odagaming29@gmail.com>
2020-05-24 17:35:38 +0300

    Add global scaling variables

---
+++ src/core/scaling.cpp
@@ -0,0 +1,4 @@
+#include"scaling.hpp"
+
+float oxu::Global::oxuPx = 0;
+oxu::Vector2<int> oxu::Global::screenSize = oxu::Vector2<int>(0,0);
---
+++ src/core/scaling.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include"../utils/vector2.hpp"
+
+namespace oxu
+{
+    struct Global
+    {
+        static float oxuPx;
+        static Vector2<int> screenSize;
+    };
+}
