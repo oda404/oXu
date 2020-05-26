cd60d113808c9010b293cf4f92ede5cfffc3f7f1
oda404 <odagaming29@gmail.com>
2020-05-27 02:21:27 +0300

    Rework vector2 class

--- src/oXu/utils/vector2.hpp
+++ src/oXu/utils/vector2.hpp
@@ -3,85 +3,152 @@
 
 #pragma once
 
+#include<cmath>
+#include<string>
+
 namespace oxu
 {
     template<typename T>
     class Vector2
     {
-    private:
-        T values[2];
-
     public:
-        Vector2() { }
+        T x, y;
 
-        Vector2(const T &firstV, const T &secondV)
+        Vector2()
         {
-            values[0] = firstV;
-            values[1] = secondV;
+
         }
 
-        const T &getX() const { return values[0]; }
- 
-        const T &getY() const { return values[1]; }
-
-        void setVector(const T &firstV, const T &secondV)
+        Vector2(const T &x, const T &y)
         {
-            values[0] = firstV;
-            values[1] = secondV;
+            this->x = x;
+            this->y = y;
         }
 
-        /* Index can only be 0 or 1 else you're going to get an out of bounds */
-        T &operator[](const unsigned int &i) { return values[i]; }
-
-        void operator=(const Vector2<T> &newVec)
+        /* Sets the x, y values of the vector to the specified ones. */
+        void setVector(const T &x, const T &y)
         {
-            values[0] = newVec.getX();
-            values[1] = newVec.getY();
+            this->x = x;
+            this->y = y;
         }
 
-        const bool operator==(const Vector2<T> &compVec) const
+        /* Returns the vector's length(magnitude). */
+        float getLength()
         {
-            return values[0] == compVec.getX() && 
-                   values[1] == compVec.getY() ?  true : false;
+            return static_cast<float>(std::sqrt(x * x + y * y));
         }
 
-        void operator+=(const Vector2<T> &pVec)
+        /* Returns the vector's length(magnitude) squared. */
+        float getLengthSquared()
         {
-            values[0] += pVec.getX();
-            values[1] += pVec.getY();
+            return static_cast<float>(x * x + y * y);
         }
 
-        void operator-=(const Vector2<T> &pVec)
+        /* Normalizes the vector. */
+        void normalize()
         {
-            values[0] -= pVec.getX();
-            values[1] -= pVec.getY();
+            *this /= this->getLength();
         }
 
-        const Vector2 operator+(const Vector2<T> &pVec) const
+        /* Returns a normalized version of the vector. This doesn't modify the base vector */
+        Vector2<T> normalized()
         {
-            Vector2 result = *this;
-            result += pVec;
+            Vector2<T> result = *this;
+            result /= this->getLength();
 
             return result;
         }
 
-        const Vector2 operator-(const Vector2<T> &pVec) const
+        /* Clamps the vector's x, y to the specified values */
+        void clamp(const T &min, const T &max)
         {
-            Vector2 result = *this;
-            result -= pVec;
+            x = x < min ? min : x;
+            x = x > max ? max : x;
+
+            y = y < min ? min : y;
+            y = y > max ? max : y;
+        }
+
+        /* Returns a clamped version of the vector. This doesn't modify the base vector */
+        Vector2<T> clamped(const T &min, const T &max)
+        {
+            Vector2<T> result = *this;
+
+            result.x = result.x < min ? min : result.x;
+            result.x = result.x > max ? max : result.x;
+
+            result.y = result.y < min ? min : result.y;
+            result.y = result.y > max ? max : result.y;
+
+            return result;
+        }
+
+        static Vector2<T> lerp(const Vector2<T> &start, const Vector2<T> &end, const float &t)
+        {
+            return start * (1 - t) + end * t;
+        }
+
+        /*
+        Returns a lerped version of the vector
+        Uses the base vector as the starting point for the lerp
+        This doesn't modify the base vector
+        */
+        Vector2<T> lerped(const Vector2<T> &dest, const float &t)
+        {
+            return *this * (1 - t) + dest * t;
+        }
+
+        /* Returns the vector formatted as a string */
+        std::string toString()
+        {
+            std::string result = "(" + std::to_string(x) + ", " + std::to_string(y) + ")";
+
+            return result;
+        }
+
+        void operator=(const Vector2<T> &v)
+        {
+            x = v.x;
+            y = v.y;
+        }
+
+        void operator+=(const Vector2<T> &v)
+        {
+            x += v.x;
+            y += v.y;
+        }
+
+        const Vector2<T> operator+(const Vector2<T> &v) const
+        {
+            Vector2<T> result = *this;
+            result += v;
+
+            return result;
+        }
+
+        void operator-=(const Vector2<T> &v)
+        {
+            x -= v.x;
+            y -= v.y;
+        }
+
+        const Vector2<T> operator-(const Vector2<T> &v) const
+        {
+            Vector2<T> result = *this;
+            result -= v;
             
             return result;
         }
 
         void operator*=(const T &n)
         {
-            values[0] *= n;
-            values[1] *= n;
+            x *= n;
+            y *= n;
         }
 
-        const Vector2 operator*(const T &n) const
+        const Vector2<T> operator*(const T &n) const
         {
-            Vector2 result = *this;
+            Vector2<T> result = *this;
             result *= n;
 
             return result;
@@ -89,26 +156,26 @@ namespace oxu
 
         void operator/=(const T &n)
         {
-            values[0] /= n;
-            values[1] /= n;
+            x /= n;
+            y /= n;
         }
 
-        const Vector2 operator/(const T &n) const
+        const Vector2<T> operator/(const T &n) const
         {
-            Vector2 result = *this;
+            Vector2<T> result = *this;
             result /= n;
 
             return result;
         }
 
-        const bool operator!=(const Vector2<T> &pVec) const
+        const bool operator==(const Vector2<T> &v) const
         {
-            if(*this == pVec)
-            {
-                return false;
-            }
+            return x == v.x && y == v.y ? true : false;
+        }
 
-            return true;
+        const bool operator!=(const Vector2<T> &v) const
+        {
+            return *this == v ? false : true;
         }
     };
 }
