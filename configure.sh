fb237458433efb390a828f43e1c3be1c0a309040
oda404 <odagaming29@gmail.com>
2020-08-06 15:45:48 +0300

    Install spdlog from configure.sh

--- configure.sh
+++ configure.sh
@@ -6,10 +6,18 @@ sudo apt install -y   \
     libsdl2-ttf-dev   \
     build-essential   \
     cmake
-    
+
+# create oXu dir the user's .config
 user_config_oXu_dir=/home/$USER/.config/oXu
 
 mkdir build $user_config_oXu_dir $user_config_oXu_dir/songs $user_config_oXu_dir/skins
 cp -r res $user_config_oXu_dir 
+
+# make and install spdlog
+cd vendor/spdlog
+mkdir build
+cd build
+cmake ..
+sudo make install -j4
 
 echo Done!
