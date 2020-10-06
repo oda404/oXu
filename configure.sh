929583ec73df974bd937aac37f5f656962d6271e
oda404 <xdxalexandru404@gmail.com>
2020-10-06 15:27:53 +0300

    Remove redundant deps

--- configure.sh
+++ configure.sh
@@ -1,9 +1,7 @@
 sudo apt update && 
 sudo apt install -y   \
     libsdl2-dev       \
-    libsdl2-image-dev \
-    libsdl2-mixer-dev \
-    libsdl2-ttf-dev   \
+    libpng-dev        \
     build-essential   \
     cmake
 
@@ -12,12 +10,5 @@ user_config_oXu_dir=/home/$USER/.config/oXu
 
 mkdir build $user_config_oXu_dir $user_config_oXu_dir/songs $user_config_oXu_dir/skins
 cp -r res $user_config_oXu_dir 
-
-# make and install spdlog
-cd vendor/spdlog
-mkdir build
-cd build
-cmake ..
-sudo make install -j4
 
 echo Done!
