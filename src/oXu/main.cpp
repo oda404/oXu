4ad6883b16a0ba796cc5b0cf27a4eb12f8c1d2b4
oda404 <xdxalexandru404@gmail.com>
2021-01-26 12:24:17 +0200

    Add exit message

--- src/oXu/main.cpp
+++ src/oXu/main.cpp
@@ -3,6 +3,7 @@
 
 #include<oXu/game.hpp>
 #include<oXu/core/status.hpp>
+#include<oXu/core/logger.hpp>
 
 /*
 Variables prefixes:
@@ -15,12 +16,18 @@ Variable suffixes:
 
 int main(void)
 {
-	if(oxu::init())
-	{
-		oxu::start();
-	}
+	if(oxu::init()) oxu::start();
 
 	oxu::clean();
+
+	if(oxu::status::get() == oxu::status::OK)
+	{
+		OXU_LOG_INFO("Exiting gracefully.");
+	}
+	else
+	{
+		OXU_LOG_INFO("Multiple memory leaks probably happened. exit code is {}", oxu::status::get());
+	}
 
 	return oxu::status::get();
 }
