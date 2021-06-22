e9e8440ec756546b2a906e6c399c42b2ccb36710
oda404 <xdxalexandru404@gmail.com>
2021-06-22 13:46:30 +0300

    Tweak oxu::Skin API and member variable names. Add oxu::Skin::get_name method.

--- include/oxu/skin/skin.hpp
+++ include/oxu/skin/skin.hpp
@@ -9,22 +9,26 @@
 
 namespace oxu
 {
-    class Skin
-    {
-    private:
-        std::string path;
-        std::string name;
-        framework::graphics::Texture textures[MAX_TEX_COUNT];
-        SDL_Cursor *customCursor = NULL;
 
-    public:
-        Skin(const std::string &path_p);
+class Skin
+{
+private:
+    std::string m_path;
+    std::string m_name;
+    framework::graphics::Texture m_textures[MAX_TEX_COUNT];
+    SDL_Cursor *m_cursor = NULL;
 
-        const framework::graphics::Texture 
-        &getTexture(uint8_t texEnum) const;
-        
-        void setCursor();
-        /* Required the renderer to be initiated */
-        void loadTextures();
-    };
+public:
+    Skin(const std::string &path);
+
+    const framework::graphics::Texture 
+    &getTexture(uint8_t texEnum) const;
+    
+    void setCursor();
+    /* Required the renderer to be initiated */
+    void loadTextures();
+
+    std::string get_name() const;
+};
+
 }
--- src/skin/skin.cpp
+++ src/skin/skin.cpp
@@ -10,78 +10,85 @@ namespace oxu
 
 using namespace framework;
 
-    Skin::Skin(const std::string &path_p):
-    path(path_p), name(path_p.substr(path_p.find_last_of('/') + 1, path_p.size() - 1))
-    {
-        OXU_LOG_DEBUG("Found skin {}", name);
-    }
-    
-    void Skin::loadTextures()
-    {
-        namespace fs = std::filesystem;
+Skin::Skin(const std::string &path)
+    : m_path(path), 
+      m_name(path.substr(path.find_last_of('/') + 1, path.size() - 1))
+{
+    OXU_LOG_DEBUG("Found skin {}", m_name);
+}
 
-        // free textures
+std::string Skin::get_name() const
+{
+    return m_name;
+}
 
-        for(const fs::directory_entry &entry: fs::directory_iterator(path))
+void Skin::loadTextures()
+{
+    namespace fs = std::filesystem;
+
+    // free textures
+
+    for(const fs::directory_entry &entry: fs::directory_iterator(m_path))
+    {
+        if(entry.path().extension() == ".png")
         {
-            if(entry.path().extension() == ".png")
+            std::string textureName = entry.path().string().substr(entry.path().string().find_last_of('/') + 1, entry.path().string().size() - 1);
+
+            std::map<std::string, std::uint8_t>::const_iterator pair = texNamesMap.find(textureName);
+
+            if(pair->first != "")
             {
-                std::string textureName = entry.path().string().substr(entry.path().string().find_last_of('/') + 1, entry.path().string().size() - 1);
-
-                std::map<std::string, std::uint8_t>::const_iterator pair = texNamesMap.find(textureName);
-
-                if(pair->first != "")
-                {
-                    textures[pair->second].load(entry.path().string());
-                }
+                m_textures[pair->second].load(entry.path().string());
             }
         }
     }
+}
 
-    void Skin::setCursor()
+void Skin::setCursor()
+{
+    graphics::Png image(m_path + "/cursor.png");
+
+    SDL_Surface *cursorSurface = 
+    SDL_CreateRGBSurfaceFrom(
+        (void*)image.getBuffer(), 
+        image.getWidth(), 
+        image.getHeight(), 
+        image.getBPP(), 
+        4 * image.getWidth(), 
+        0x000000ff, 
+        0x0000ff00, 
+        0x00ff0000, 
+        0xff000000
+    );
+
+    if(!cursorSurface)
     {
-        graphics::Png image(path + "/cursor.png");
-
-        SDL_Surface *cursorSurface = 
-        SDL_CreateRGBSurfaceFrom(
-            (void*)image.getBuffer(), 
-            image.getWidth(), 
-            image.getHeight(), 
-            image.getBPP(), 
-            4 * image.getWidth(), 
-            0x000000ff, 
-            0x0000ff00, 
-            0x00ff0000, 
-            0xff000000
-        );
-
-        if(!cursorSurface)
-        {
-            OXU_LOG_WARN(SDL_GetError());
-            return;
-        }
-
-        if(customCursor)
-        {
-            SDL_FreeCursor(customCursor);
-            customCursor = NULL;
-        }
-
-        customCursor = SDL_CreateColorCursor(cursorSurface, cursorSurface->w / 2, cursorSurface->h / 2);
-
-        SDL_FreeSurface(cursorSurface);
-
-        SDL_SetCursor(customCursor);
+        OXU_LOG_WARN(SDL_GetError());
+        return;
     }
 
-    const graphics::Texture &Skin::getTexture(uint8_t texNum) const
+    if(m_cursor)
     {
-        if(texNum >= MAX_TEX_COUNT)
-        {
-            OXU_LOG_WARN("Tried to access a non existent texture, returned last texture in array!");
-            return textures[MAX_TEX_COUNT - 1];
-        }
-
-        return textures[texNum];
+        SDL_FreeCursor(m_cursor);
+        m_cursor = NULL;
     }
+
+    m_cursor = SDL_CreateColorCursor(cursorSurface, cursorSurface->w / 2, cursorSurface->h / 2);
+
+    SDL_FreeSurface(cursorSurface);
+
+    SDL_SetCursor(m_cursor);
+}
+
+const graphics::Texture &Skin::getTexture(uint8_t texNum) const
+{
+    if(texNum >= MAX_TEX_COUNT)
+    {
+        OXU_LOG_WARN("Tried to access a non existent texture, returned last texture in array!");
+        return m_textures[MAX_TEX_COUNT - 1];
+    }
+
+    return m_textures[texNum];
+}
+
 }
